85ecec59196263865284fc4c2c2ae6a2c11caaba
Naufal <m.naufal.fadhillah98@gmail.com>
2022-02-07 11:43:56 +0700

    clean architecture

---
+++ app/src/main/java/com/dicoding/tourismapp/core/domain/usecase/TourismInteractor.kt
@@ -0,0 +1,20 @@
+package com.dicoding.tourismapp.core.domain.usecase
+
+import androidx.lifecycle.LiveData
+import com.dicoding.tourismapp.core.data.Resource
+import com.dicoding.tourismapp.core.domain.model.Tourism
+import com.dicoding.tourismapp.core.domain.repository.ITourismRepository
+
+class TourismInteractor(private val tourismRepository: ITourismRepository): TourismUseCase {
+    override fun getAllTourism(): LiveData<Resource<List<Tourism>>> {
+        return tourismRepository.getAllTourism()
+    }
+
+    override fun getFavoriteTourism(): LiveData<List<Tourism>> {
+        return tourismRepository.getFavoriteTourism()
+    }
+
+    override fun setFavoriteTourism(tourism: Tourism, state: Boolean) {
+        tourismRepository.setFavoriteTourism(tourism, state)
+    }
+}
