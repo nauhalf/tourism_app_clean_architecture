b71e2431da20e1babea3587ed9f2cfa8b691e2bc
Naufal <m.naufal.fadhillah98@gmail.com>
2022-02-08 11:37:09 +0700

    Update: implement dagger injection

--- app/src/main/java/com/dicoding/tourismapp/core/di/NetworkModule.kt
+++ app/src/main/java/com/dicoding/tourismapp/core/di/NetworkModule.kt
@@ -1,13 +1,20 @@
-package com.dicoding.tourismapp.core.data.source.remote.network
+package com.dicoding.tourismapp.core.di
 
+import com.dicoding.tourismapp.core.data.source.remote.network.ApiService
+import dagger.Module
+import dagger.Provides
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import java.util.concurrent.TimeUnit
+import javax.inject.Singleton
 
-object ApiConfig {
+@Module
+class NetworkModule {
 
+    @Singleton
+    @Provides
     fun provideOkHttpClient(): OkHttpClient {
         return OkHttpClient.Builder()
             .addInterceptor(HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY))
@@ -16,6 +23,7 @@ object ApiConfig {
             .build()
     }
 
+    @Provides
     fun provideApiService(): ApiService {
         val retrofit = Retrofit.Builder()
             .baseUrl("https://tourism-api.dicoding.dev/")
